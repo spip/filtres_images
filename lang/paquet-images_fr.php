9f9b2c68114b474e3d5fbf3f4174e06b841c8057
salvatore@rezo.net <>
2020-02-03 12:00:35 +0000

    [Salvatore] [source: paquet-images] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: paquet-images] Mise a jour du bilan depuis https://trad.spip.net

--- lang/paquet-images_fr.php
+++ lang/paquet-images_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/filtres_images/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/filtres_images/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
