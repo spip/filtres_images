9ffdfc003af4878e0c68db5c536de1516aa09aab
Matthieu Marcillaud <marcimat@rezo.net>
2021-03-22 15:02:26 +0100

    Ne dupliquons pas du code qu'on a envoyé dans SPIP Core (sous un autre nom). Cf PR du Core #141 et commits du core daf134ca7 et 761b91888
    Todo: remplacer l'usage de ces fonctions deprecated par leur équivalent.

--- filtres/images_lib.php
+++ filtres/images_lib.php
@@ -157,132 +157,34 @@ function _couleur_hsv2rgb($H, $S, $V) {
 
 /**
  * Transformation d'une couleur RGB en HSL
+ * 
  * HSL float entre 0 et 1
  * RGB entiers entre 0 et 255
  *
+ * @deprecated use _couleur_rgb_to_hsl()
  * @param int $R
  * @param int $G
  * @param int $B
  * @return array
  */
 function _couleur_rgb2hsl($R, $G, $B) {
-	$var_R = ($R / 255);                    //Where RGB values = 0 Ã· 255
-	$var_G = ($G / 255);
-	$var_B = ($B / 255);
-
-	$var_Min = min($var_R, $var_G, $var_B);   //Min. value of RGB
-	$var_Max = max($var_R, $var_G, $var_B);   //Max. value of RGB
-	$del_Max = $var_Max - $var_Min;           //Delta RGB value
-
-	$L = ($var_Max + $var_Min) / 2;
-
-	if ($del_Max == 0)                     //This is a gray, no chroma...
-	{
-		$H = 0;                            //HSL results = 0 Ã· 1
-		$S = 0;
-	} else                                    //Chromatic data...
-	{
-		if ($L < 0.5) {
-			$S = $del_Max / ($var_Max + $var_Min);
-		} else {
-			$S = $del_Max / (2 - $var_Max - $var_Min);
-		}
-
-		$del_R = ((($var_Max - $var_R) / 6) + ($del_Max / 2)) / $del_Max;
-		$del_G = ((($var_Max - $var_G) / 6) + ($del_Max / 2)) / $del_Max;
-		$del_B = ((($var_Max - $var_B) / 6) + ($del_Max / 2)) / $del_Max;
-
-		if ($var_R == $var_Max) {
-			$H = $del_B - $del_G;
-		} else {
-			if ($var_G == $var_Max) {
-				$H = (1 / 3) + $del_R - $del_B;
-			} else {
-				if ($var_B == $var_Max) {
-					$H = (2 / 3) + $del_G - $del_R;
-				}
-			}
-		}
-
-		if ($H < 0) {
-			$H += 1;
-		}
-		if ($H > 1) {
-			$H -= 1;
-		}
-	}
-
-	$ret["h"] = $H;
-	$ret["s"] = $S;
-	$ret["l"] = $L;
-
-	return $ret;
+	return _couleur_rgb_to_hsl($R, $G, $B);
 }
-
-/**
- * Calcul d'une composante R, G ou B
- *
- * @param unknown_type $v1
- * @param unknown_type $v2
- * @param unknown_type $vH
- * @return float
- */
-function hue_2_rgb($v1, $v2, $vH) {
-	if ($vH < 0) {
-		$vH += 1;
-	}
-	if ($vH > 1) {
-		$vH -= 1;
-	}
-	if ((6 * $vH) < 1) {
-		return ($v1 + ($v2 - $v1) * 6 * $vH);
-	}
-	if ((2 * $vH) < 1) {
-		return ($v2);
-	}
-	if ((3 * $vH) < 2) {
-		return ($v1 + ($v2 - $v1) * ((2 / 3) - $vH) * 6);
-	}
-
-	return ($v1);
-}
-
 
 /**
  * Transformation d'une couleur HSL en RGB
+ * 
  * HSL float entre 0 et 1
  * RGB entiers entre 0 et 255
  *
+ * @deprecated use _couleur_hsl_to_rgb()
  * @param float $H
  * @param float $S
  * @param float $L
  * @return array
  */
 function _couleur_hsl2rgb($H, $S, $L) {
-
-	if ($S == 0)                       //HSV values = 0 -> 1
-	{
-		$R = $L * 255;
-		$G = $L * 255;
-		$B = $L * 255;
-	} else {
-		if ($L < 0.5) {
-			$var_2 = $L * (1 + $S);
-		} else {
-			$var_2 = ($L + $S) - ($S * $L);
-		}
-
-		$var_1 = 2 * $L - $var_2;
-
-		$R = 255 * hue_2_rgb($var_1, $var_2, $H + (1 / 3));
-		$G = 255 * hue_2_rgb($var_1, $var_2, $H);
-		$B = 255 * hue_2_rgb($var_1, $var_2, $H - (1 / 3));
-	}
-	$ret["r"] = floor($R);
-	$ret["g"] = floor($G);
-	$ret["b"] = floor($B);
-
-	return $ret;
+	return _couleur_hsl_to_rgb($H, $S, $L);
 }
 
 // A partir d'une image,
